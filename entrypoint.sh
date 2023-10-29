518ef9732c5b80e85edbff85e12dcee75cc5c234
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2023-10-29 13:59:08 +0100

    Add output parameter and job summary

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
 echo "Hello $@"
+echo "answer=42" >> $GITHUB_OUTPUT
+
+echo "### Hello $@! :rocket:" >> $GITHUB_STEP_SUMMARY
+echo "<h3> The answer from Deep Thought is 42 :robot:</h3>" >> $GITHUB_STEP_SUMMARY
